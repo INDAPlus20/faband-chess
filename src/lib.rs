9e81d52e2c09e6de6092cb235d17f20ce6cc17c1
NAIBAFFF <fab.andreasson@gmail.com>
2020-09-15 16:40:47 +0200

    Added template so generously given to us by Viola

---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+use std::fmt;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum GameState {
+    InProgress,
+    Check,
+    GameOver,
+}
+
+pub struct Game {
+    ///Save board, active color, ...
+    state: GameState,
+}
+
+impl Game {
+    ///Initialises a new board with pieces in start positions
+    pub fn new() -> Game {
+        Game {
+            ///Initialises a new board and set the active color to white
+            state: GameState::InProgress,
+        }
+    }
+    ///If the current game state is in progress and the move is legal
+    ///then move a piece and return the resulting state of the game
+    pub fn make_move(&mut self, _from: String, _to: String) -> Option<GameState> {
+        None
+    }
+
+    ///Set the piece type that a peasant becomes following a promotion
+    pub fn set_promotion(&mut self, _piece: String) -> () {
+        ()
+    }
+
+    ///Get the current game state
+    pub fn get_game_state(&self) -> GameState {
+        self.state
+    }
+
+    ///If a piece is standing on a tile, return all possible moves for
+    ///that piece. Include rules for check. Include special moves (en passent, etc)
+    pub fn get_possible_moves(&self, _position: String) -> Option<Vec<String>> {
+        None
+    }
+}
+
+/// Implement print routine for Game.
+///
+/// Output example:
+/// |:----------------------:|
+/// | R  Kn B  K  Q  B  Kn R |
+/// | P  P  P  P  P  P  P  P |
+/// | *  *  *  *  *  *  *  * |
+/// | *  *  *  *  *  *  *  * |
+/// | *  *  *  *  *  *  *  * |
+/// | *  *  *  *  *  *  *  * |
+/// | P  P  P  P  P  P  P  P |
+/// | R  Kn B  K  Q  B  Kn R |
+/// |:----------------------:|
+impl fmt::Debug for Game {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        /* build board representation string */
+        write!(f, "")
+    }
+}
+
+// --------------------------
+// ######### TESTS ##########
+// --------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::Game;
+    use super::GameState;
+
+    // check test framework
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    // example test
+    // check that game state is in progress after initialisation
+    #[test]
+    fn game_in_progress_after_init() {
+        let game = Game::new();
+
+        assert_eq!(game.get_game_state(), GameState::InProgress);
+    }
+}
